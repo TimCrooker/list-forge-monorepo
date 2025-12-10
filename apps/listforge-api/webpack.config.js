360f7e3a1814e7980fc3a7ac84c5d9ce606b9bb4
Tim Crooker <timothycrooker@gmail.com>
2025-12-10 09:04:37 -0500

    Fix webpack config to bundle workspace packages
    - Configure externals to bundle @listforge/* packages
    - Only externalize node_modules, not workspace deps
    - This ensures @listforge packages are bundled in dist

--- apps/listforge-api/webpack.config.js
+++ apps/listforge-api/webpack.config.js
@@ -1,6 +1,20 @@
 module.exports = function (options, webpack) {
   return {
     ...options,
+    externals: [
+      // Externalize all node_modules EXCEPT workspace packages
+      function ({ request }, callback) {
+        if (/^@listforge\//.test(request)) {
+          // Bundle workspace packages
+          return callback();
+        }
+        // Externalize everything else
+        if (/^[a-z@][a-z0-9-@\/]*$/.test(request)) {
+          return callback(null, 'commonjs ' + request);
+        }
+        callback();
+      },
+    ],
     optimization: {
       ...options.optimization,
       minimize: false,
