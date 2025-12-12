fc7ee75d7a24b34d06cd75bd159de3394b3c254a
Tim Crooker <timothycrooker@gmail.com>
2025-12-12 09:10:33 -0500

    fix: Replace broken Clearbit logo URLs with Wikimedia Commons links and add Etsy/Poshmark logos

--- apps/listforge-landing/src/data/logos.ts
+++ apps/listforge-landing/src/data/logos.ts
@@ -14,17 +14,27 @@ export const listForgeLogo = {
 export const marketplaceLogos: Logo[] = [
   {
     name: 'eBay',
-    src: 'https://logo.clearbit.com/ebay.com',
+    src: 'https://upload.wikimedia.org/wikipedia/commons/1/1b/EBay_logo.svg',
     href: 'https://www.ebay.com',
   },
   {
     name: 'Amazon',
-    src: 'https://logo.clearbit.com/amazon.com',
+    src: 'https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg',
     href: 'https://www.amazon.com',
   },
   {
     name: 'Facebook Marketplace',
-    src: 'https://logo.clearbit.com/facebook.com',
+    src: 'https://upload.wikimedia.org/wikipedia/commons/5/5c/Facebook_Marketplace_Logo.png',
     href: 'https://www.facebook.com/marketplace',
+  },
+  {
+    name: 'Etsy',
+    src: 'https://upload.wikimedia.org/wikipedia/commons/8/8b/Etsy_logo.svg',
+    href: 'https://www.etsy.com',
+  },
+  {
+    name: 'Poshmark',
+    src: 'https://upload.wikimedia.org/wikipedia/commons/3/3e/Poshmark_logo.png',
+    href: 'https://www.poshmark.com',
   },
 ]
